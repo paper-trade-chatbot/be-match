4c483d8ab02883604e8a409bfa98737c24f61a69
lisyaoran51 <lisyaoran51@hotmail.com>
2023-05-09 08:02:43 -0700

    update

--- main.go
+++ main.go
@@ -18,31 +18,17 @@ import (
 
 func main() {
 
-	// We're running, turn on the liveness indication flag.
 	global.Alive = true
 
-	// Create root context.
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	// Setup logging module.
-	// NOTE: This should always be first.
 	logging.Initialize(ctx)
 	defer logging.Finalize()
 
-	// // Setup redis
 	cache.Initialize(ctx)
 	defer cache.Finalize()
 
-	// // Setup quote redis
-	// redisQuote.Initialize(ctx)
-	// defer redisQuote.Finalize()
-
-	// // Setup cache module.
-	// redisCluster.Initialize(ctx)
-	// defer redisCluster.Finalize()
-
-	// // Setup database module.
 	database.Initialize(ctx)
 	defer database.Finalize()
 
@@ -54,51 +40,15 @@ func main() {
 
 	initConfig()
 
-	// grpcAddress := fmt.Sprintf("%s:%s",
-	// 	config.GetString("GRPC_SERVER_LISTEN_ADDRESS"),
-	// 	config.GetString("GRPC_SERVER_LISTEN_PORT"))
-	// listener := server.CreateGRpcServer(ctx, grpcAddress)
-
-	// recoveryOpt := grpc_recovery.WithRecoveryHandlerContext(
-	// 	func(ctx context.Context, p interface{}) error {
-	// 		logging.Error(ctx, "[PANIC] %s\n\n%s", p, string(debug.Stack()))
-	// 		return status.Errorf(codes.Internal, "%s", p)
-	// 	},
-	// )
-	// grpc := grpc.NewServer(
-	// 	grpc.StreamInterceptor(grpc_middleware.ChainStreamServer(
-	// 		grpc_recovery.StreamServerInterceptor(recoveryOpt),
-	// 	)),
-	// 	grpc.UnaryInterceptor(grpc_middleware.ChainUnaryServer(
-	// 		grpc_recovery.UnaryServerInterceptor(recoveryOpt),
-	// 	)),
-	// )
-	// reflection.Register(grpc)
-
-	// orderInstance := order.New()
-	// orderGrpc.RegisterOrderServiceServer(grpc, orderInstance)
-
-	// Create HTTP server instance to listen on all interfaces.
 	address := fmt.Sprintf("%s:%s",
 		config.GetString("SERVER_LISTEN_ADDRESS"),
 		config.GetString("SERVER_LISTEN_PORT"))
 	httpServer := server.CreateHttpServer(ctx, address)
 
-	// run cron job
 	go cronjob.Cron()
 
-	// go func() {
-	// 	logging.Info(ctx, "grpc serving")
-	// 	if err := grpc.Serve(*listener); err != nil {
-	// 		panic(err)
-	// 	}
-	// }()
-
-	// Now that we finished initializing all necessary modules,
-	// let's turn on the readiness indication flag.
 	global.Ready = true
 
-	// Start servicing requests.
 	logging.Info(ctx, "Initialization complete, listening on %s...", address)
 	if err := httpServer.ListenAndServe(); err != nil {
 		logging.Info(ctx, err.Error())
